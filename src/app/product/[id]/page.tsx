c6e20355f48a2fc372090d87b7e79dcc2ec2ebb1
chaim baror <chbaror@gmail.com>
2025-11-24 22:38:07 +0200

    Enhance localization and improve product page loading experience
    - Added new localization messages for "Select Color" and "Select Size" in both English and Hebrew files to enhance user experience.
    - Updated the `ProductPage` component to include a loading skeleton while fetching product details, improving perceived performance and user feedback.
    - Refactored the `ProductPageSkeleton` to include a Navbar and Footer for a more complete layout during loading.

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { detailedProducts, getDetailedProduct } from '@/lib/data/data';
+import React, { Suspense } from 'react';
+import { getDetailedProduct } from '@/lib/data/data';
 import ProductPageClient from '@/components/Product/ProductPageClient';
 import ProductNotFound from '@/components/Product/ProductNotFound';
-import { Product } from '@/types';
-
+import ProductPageSkeleton from '@/components/Shared/Skeletons/ProductPageSkeleton';
 interface ProductPageProps {
   params: Promise<{
     id: string;
@@ -14,10 +13,14 @@ export default async function ProductPage({ params }: ProductPageProps) {
   const resolvedParams = await params;
   const product = getDetailedProduct(resolvedParams.id);
 
+  
   if (!product) {
     return <ProductNotFound />;
   }
 
-
-  return <ProductPageClient product={product} />;
+  return (
+    <Suspense fallback={<ProductPageSkeleton />}> 
+      <ProductPageClient product={product} />
+    </Suspense>
+  );
 }
